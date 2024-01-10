9fb23dbfc09b55aca6eb8b9b2f1a82c017619326
bidhata <pandey.bidhata58@gmail.com>
2024-01-10 10:57:23 +0545

    responsiveness work

--- home/index.php
+++ home/index.php
@@ -2,6 +2,17 @@
 <html lang="en">
 <head>
   <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Home</title>
+</head>
+<body>
+  
+</body>
+</html>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
   <title>My Website</title>
   <link rel="stylesheet" href="../css/style.css">
   <script src="https://cdn.jsdelivr.net/particles.js/2.0.0/particles.min.js"></script>
